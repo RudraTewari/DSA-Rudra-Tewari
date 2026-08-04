8b351f3863bcbdc307444278e161118056ef5c55
Rudra Tewari <rudratewari30@gmail.com>
2026-08-04 13:00:09 +0530

    Refactor SkipCharacter class to change method signature and improve character skipping functionality

--- Recursion/SkipCharacter.java
+++ Recursion/SkipCharacter.java
@@ -1,21 +1,24 @@
 
 
 public class SkipCharacter {
-    public static String skipCharacter(String str, char ch,int idx){
-        if(idx == str.length()){
-            return "";
+    public static void skipCharacter(String processed, String unProcessed,char ch){
+        if(unProcessed.isEmpty()){
+            System.out.print(processed);
+            System.out.println();
+            return;
         }
-        if(str.charAt(idx)==ch){
-            return skipCharacter(str,ch,idx+1);
+        char c = unProcessed.charAt(0);
+
+        if(c == ch){
+            skipCharacter(processed, unProcessed.substring(1), ch);
+        }else{
+            skipCharacter(processed+c, unProcessed.substring(1), ch);
         }
-        String ans = skipCharacter(str,ch,idx+1);
-        return str.charAt(idx)+ans;
+
     }
     public static void main(String[] args) {
-        char ch = 'b';
+        char ch = 'a';
         String str = "baccad";
-        int idx = 0;
-        String ans = skipCharacter(str,ch,idx);
-        System.out.println(ans);
+        skipCharacter("",str,ch);
     }
 }
