744317ee189d1bf9ef64d94b1802b731d92df45d
Rudra Tewari <rudratewari30@gmail.com>
2026-08-05 12:59:11 +0530

    Add IntegerPermutation class to generate all permutations of a given integer array

---
+++ Recursion/IntegerPermutation.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class IntegerPermutation {
+    static void findPermutations(ArrayList<Integer> processed,int[] unProcessed){
+        if(unProcessed.length==0){
+            System.out.println(processed);
+            return;
+        }
+
+        int current = unProcessed[0];
+        int[] rem = Arrays.copyOfRange(unProcessed, 1, unProcessed.length);
+        for (int index = 0; index <= processed.size(); index++) {
+            processed.add(index,current);
+            findPermutations(processed, rem);
+            processed.remove(index);
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+
+        findPermutations(new ArrayList<>(),arr);
+    }
+}
