e919bde388a7f8dddb3155925282f4f7ddeee505
Rudra Tewari <rudratewari30@gmail.com>
2026-08-19 11:54:59 +0530

    Refactor CountingPaths class to improve pathfinding logic and add pathRestrictions method for maze traversal

--- Backtracking/CountingPaths.java
+++ Backtracking/CountingPaths.java
@@ -2,17 +2,17 @@ package Backtracking;
 
 public class CountingPaths{
     public static int findPaths(int[][] mat,int row,int col,int targetRow,int targetCol){
-        if(row == targetRow-1&&col==targetCol-1){
+        if(row == targetRow-1 && col==targetCol-1){
             return 1;
         }
         int downCount = 0;
         int rightCount = 0;
 
-        if (row < targetRow - 1) {
+        if (row < targetRow-1) {
             downCount = findPaths(mat, row + 1, col, targetRow, targetCol);
         }
 
-        if (col < targetCol - 1) {
+        if (col < targetCol-1) {
             rightCount = findPaths(mat, row, col + 1, targetRow, targetCol);
         }
 
@@ -77,10 +77,37 @@ public class CountingPaths{
             path.deleteCharAt(path.length()-1);
         }
     }
+
+    public static void pathRestrictions(int[][] mat,int row,int col,int targetRow,
+                                        int targetCol, boolean[][] maze, StringBuilder path){
+        if(row==targetRow-1 && col == targetCol-1){
+            System.out.println(path);
+            return;
+        }
+        if(!maze[row][col]){
+            return;
+        }
+
+        if(row<targetRow-1){
+            path.append("D");
+            pathRestrictions(mat, row+1, col, targetRow, targetCol, maze, path);
+            path.deleteCharAt(path.length()-1);
+        }
+        if(col < targetCol-1){
+            path.append("R");
+            pathRestrictions(mat, row, col+1, targetRow, targetCol, maze, path);
+            path.deleteCharAt(path.length()-1);
+        }
+    }
     public static void main(String[] args){
         int[][] mat = {{1,2,3},{2,3,4},{3,4,5}};
-        int targetRow=3;
+        int targetRow=2;
         int targetCol=3;
+        boolean[][] maze= {
+            {true, true,true},
+            {true,false,true},
+            {true,true,true}
+        };
         int pathCount = findPaths(mat,0,0,targetRow,targetCol);
         StringBuilder path = new StringBuilder("");
         findPathsII(mat,0,0,targetRow,targetCol,path);
@@ -89,5 +116,8 @@ public class CountingPaths{
         System.out.println("Answer with Diagonal : "+pathCountwithDiagonal);
         StringBuilder pathI=new StringBuilder("");
         findPathsWithDiagonalII(mat, 0, 0, targetRow, targetCol, pathI);
+        System.out.println("---------------------------------------------------");
+        StringBuilder pathII=new StringBuilder("");
+        pathRestrictions(mat, 0, 0, targetRow, targetCol, maze, pathII);
     }
 }
